4099c57a82974d405436e10b048ee1c54832f67c
ekkasitconvergence <ekkasit.convergence22@gmail.com>
2019-05-01 14:48:59 +0700

    form of register

---
+++ frm_register.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<link rel="stylesheet" href="style2.css">
+
+<body>
+ <section> 
+         <div class="a">
+            <form action="register.php" method="post">
+               <h1>REGISTER HERE</h1>
+               <label>username</label>
+               <input type="text" name="username" required>
+               <label>password</label>
+               <input type="password" name="password" required >
+               <label>email</label>
+               <input type="email" name="email" required >
+               <br><br>
+               <input type="submit"  name="submit" value="register">
+          </form>  
+          <a href="login.php">กลับสู่หน้าสlogin</a>
+          </div> 
+</section>
+</body>
+</html>
